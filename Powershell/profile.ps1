3b6a4d6c9bae63ef76452d95cd0d2f1fd4e91cf7
Max Lefebvre <max.lefebvre@garvin-allen.com>
2018-09-10 14:05:49 -0300

    conemu, jetbrains, powershell & visual studio settings added

---
+++ Powershell/profile.ps1
@@ -0,0 +1,66 @@
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt {
+    # https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
+    $origLastExitCode = $LastExitCode
+    Write-VcsStatus
+
+    if (Test-Administrator) {  # if elevated
+        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+    }
+
+    Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+    Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    }
+
+    Write-Host $curPath -NoNewline -ForegroundColor Blue
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    $LastExitCode = $origLastExitCode
+    "`n$('>' * ($nestedPromptLevel + 1)) "
+}
+
+Import-Module posh-git
+Import-Module PSColor
+
+$global:PSColor = @{
+    File = @{
+        Default    = @{ Color = 'White' }
+        Directory  = @{ Color = 'Cyan'}
+        Hidden     = @{ Color = 'DarkGray'; Pattern = '^\.' } 
+        Code       = @{ Color = 'Magenta'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
+        Executable = @{ Color = 'Red'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$' }
+        Text       = @{ Color = 'Yellow'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
+        Compressed = @{ Color = 'Green'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
+    }
+    Service = @{
+        Default = @{ Color = 'White' }
+        Running = @{ Color = 'DarkGreen' }
+        Stopped = @{ Color = 'DarkRed' }     
+    }
+    Match = @{
+        Default    = @{ Color = 'White' }
+        Path       = @{ Color = 'Cyan'}
+        LineNumber = @{ Color = 'Yellow' }
+        Line       = @{ Color = 'White' }
+    }
+    NoMatch = @{
+        Default    = @{ Color = 'White' }
+        Path       = @{ Color = 'Cyan'}
+        LineNumber = @{ Color = 'Yellow' }
+        Line       = @{ Color = 'White' }
+    }
+}
+
+$global:GitPromptSettings.BeforeText = '['
+$global:GitPromptSettings.AfterText  = '] '
